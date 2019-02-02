5c92c47541c2118f8a2d1431febe717078a5874c
Russell Graves/Syonyk <rgraves@sevarg.net>
2019-02-01 20:40:06 -0700

    Fix up config file formatting, remove non-MySQL options.

--- htdocs/config/config-main-skel.php
+++ htdocs/config/config-main-skel.php
@@ -1,38 +1,31 @@
 <?php
 
-/** 
+/**
  *  CONFIG-MAIN
  *
  *  black box config 
  * 
- *  @revision $Rev$
- *  @author: peter 2013
+ *  @revision 1.0
+ *  @author: peter 2013/Syonyk 2019
  *  @license: GPLv3. Use at your own risk. 
  *
- *
- **/ 
-
-
+ * */
 #-------------------MAIN CONFIGURATION--------------------#
-
-
 ### Database config
-
-$SQL_TYPE=  'MYSQL';
-$SQL_HOST=  'localhost';            
-$SQL_DB=    'blackbox';      
-$SQL_USER=  'cubie';           
-$SQL_PASS=  'secret';              
+// MYSQL is the only DB currently supported.
+$SQL_TYPE = 'MYSQL';
+$SQL_HOST = 'localhost';
+$SQL_DB = 'blackbox';
+$SQL_USER = 'cubie';
+$SQL_PASS = 'secret';
 
 
 
 ### Global settings
-
-//this is the time interval in minutes that the device data is stored in the database
+//this is the time interval in minutes that the device data is stored in the 
+//database
 //1-15 minutes is about right, minimum 1 minute
-
-$SETTINGS['sample_interval']= 1; //minutes
-
+$SETTINGS['sample_interval'] = 1; //minutes
 
 //the code stores the current days data in a verbose form
 //set where you want the temp data files to be stored
@@ -41,26 +34,19 @@ $SETTINGS['sample_interval']= 1; //minutes
 //bb adds a folder called blackbox so no need to add that to the path
 //ramdisks start empty anyway
 //no trailing slash, must exist
-
-$SETTINGS['temp_data_dir']= '/var/tmp'; 
-
+$SETTINGS['temp_data_dir'] = '/var/tmp';
 
 //set where you want the permanent data files to be stored
 //these are bziped daily records from yesterday onwards
 //uses a /data/modulename/yyyy-mm-dd.csv.bz2 tree format
 //no trailing slash, must exist
-
-$SETTINGS['data_dir']= '/home/blackbox'; 
+$SETTINGS['data_dir'] = '/home/blackbox';
 
 
 //set where the blackbox bin directory is located
 //these are for scripts and binarys that run out of cron, or in the background
 //no trailing slash, must exist
-
-$SETTINGS['bin_dir']= '/opt/blackbox'; 
-
-
+$SETTINGS['bin_dir'] = '/opt/blackbox';
 
 #------------------ END CONFIGURATION----------------------#
-
 ?>
